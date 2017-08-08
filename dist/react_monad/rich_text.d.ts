47e234bf76d6bac366bec26bc926f3cd3cb89997
giuseppemag <giuseppemag@gmail.com>
2017-08-08 11:37:15 +0200

    Compiled dist.

--- dist/react_monad/rich_text.d.ts
+++ dist/react_monad/rich_text.d.ts
@@ -1,3 +1,3 @@
 import { C, Mode } from './core';
-export declare type MediaType = 'image' | 'video' | 'youtube';
+export declare type MediaType = 'image' | 'video' | 'youtube' | 'mathblock';
 export declare function rich_text(mode: Mode, key?: string, dbg?: () => string): (_: string) => C<string>;
