a031526ec1c67b6f2ab78e078934beb35c3b9bd6
pavelkucherov <30284900+pavelkucherov@users.noreply.github.com>
2017-12-04 13:38:09 +0100

    Add Memoizer component

---
+++ samples/Client/samples/memoizer.tsx
@@ -0,0 +1,20 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import {List, Map, Set} from "immutable"
+import * as Immutable from "immutable"
+import {UrlTemplate, application, get_context, Route, Url, make_url, fallback_url, link_to_route,
+Option, C, Mode, unit, bind, string, number, bool, button, selector, multi_selector, label, h1, h2, div, form, image, link, file, overlay,
+custom, repeat, all, any, lift_promise, retract, delay,
+simple_menu, mk_menu_entry, mk_submenu_entry, MenuEntry, MenuEntryValue, MenuEntrySubMenu,
+rich_text, paginate, Page, list, editable_list} from '../../../src/monadic_react'
+
+
+export let memoizer_sample : C<void> =
+  repeat<number>(`input number`)(n =>
+      label<number, number>("Insert an even number: ", true)(n =>
+        number("edit", "number")(n))(n))(0).then(`input number bind`, n =>
+  button<number>(`Send ${n.toString()} further`, false)(n)
+  .then("",undefined)
+  .map<string>(n =>
+  `Your selection is ${n.toString()}`).then(`button to string`, s =>
+  string("view")(s).ignore()))
---
+++ src/react_monad/memoizer.tsx
@@ -0,0 +1,38 @@
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+import * as Immutable from "immutable"
+import * as Moment from "moment"
+import {C, unit, bind, Mode, make_C, CmdCommon} from '../react_monad/core'
+import {string, number, bool} from '../react_monad/primitives'
+import {button, selector, multi_selector, label, image} from '../react_monad/html'
+import {custom, repeat, any, lift_promise, retract, delay, simple_menu, hide} from '../react_monad/combinators'
+
+type MemoizerProps<A,B> = {
+  kind:"memoizer",
+  input: () => C<B>,
+  time: Moment.Moment
+} & CmdCommon<B>
+
+type MemoizerState<A,B> = {  }
+
+class Memoizer<A,B> extends React.Component<MemoizerProps<A,B>,MemoizerState<A,B>> {
+  constructor(props:MemoizerProps<A,B>,context:any) {
+    super(props, context)
+    
+  }
+  componentWillReceiveProps(new_props:MemoizerProps<A,B>) {
+    
+  }
+  componentWillMount() {
+    
+  }
+  render() {
+    return undefined
+  }
+}
+
+export let memoizer = function<A,B>(input:() => C<B>, time?:Moment.Moment, key?:string, dbg?:() => string) :C<B> {
+  return make_C<B>(context => cont =>
+        React.createElement<MemoizerProps<A,B>>(Memoizer,
+          { kind:"memoizer", input:input, time: time,  cont:cont, context:context, key:key,  debug_info:dbg }))
+}
