261144fd9f45f95f05b3c24f7ba71e3e0324b460
Niels van der Veer <n.i.vdveer@gmail.com>
2018-02-08 13:11:38 +0100

    Repair the RichText sample

--- samples/wwwroot/js/site.js
+++ samples/wwwroot/js/site.js
@@ -37978,7 +37978,7 @@ const getResponse = (request) => {
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const monadic_react_1 = __webpack_require__(15);
-exports.rich_text_sample = monadic_react_1.repeat()(monadic_react_1.rich_text("edit"))(null).map(s => s.substr(0, 1000)).then(`rich text sample`, monadic_react_1.delay(2000, `rich text delayer`)(monadic_react_1.label(`Raw content:`, true)(monadic_react_1.string("view", "text", "view")))).ignore();
+exports.rich_text_sample = monadic_react_1.repeat()(monadic_react_1.rich_text("edit"))('').map(s => s.substr(0, 1000)).then(`rich text sample`, monadic_react_1.delay(2000, `rich text delayer`)(monadic_react_1.label(`Raw content:`, true)(monadic_react_1.string("view", "text", "view")))).ignore();
 
 
 /***/ }),
