911596f3d2bdf6d1f87d5b4dc7fc36b542ead185
Thomas Santoli <thomassantoli@hoppinger.com>
2018-03-02 09:56:34 +0100

    Restore samples as they were

--- samples/Client/samples/label.tsx
+++ samples/Client/samples/label.tsx
@@ -8,17 +8,7 @@ custom, repeat, all, any, lift_promise, retract, delay,
 simple_menu, mk_menu_entry, mk_submenu_entry, MenuEntry, MenuEntryValue, MenuEntrySubMenu,
 rich_text, paginate, Page, list, editable_list} from '../../../src/monadic_react'
 
-export let label_sample : C<void> = any<string,string>()([
-  _ => only_label.never('byitself'),
-  repeat<string>('tomakesometests')(string('edit','text','justaninputfield')),
-  string('edit','text-send','try-to-send-text')
-])('hello')
-.then<string>('display-what-I-write',string('view','text','established'))
-.ignore()
-
-
-
-const only_label : C<void> =
+export let label_sample : C<void> =
   repeat<number>(`input number`)(n =>
       label<number, number>("Insert a number: ", true)(n =>
         number("edit", "number")(n))(n))(0).then(`input number bind`, c =>
